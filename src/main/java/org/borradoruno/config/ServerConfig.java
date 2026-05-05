3af9fdc05b01570bd45e809b652f41c9e8b2afd3
AlexNieblas10 <elalexnieblas@gmail.com>
2026-05-05 07:13:56 -0700

    feat: change server to fly.io

--- src/main/java/org/borradoruno/config/ServerConfig.java
+++ src/main/java/org/borradoruno/config/ServerConfig.java
@@ -10,8 +10,8 @@ public class ServerConfig {
     private static final Preferences prefs = Preferences.userNodeForPackage(ServerConfig.class);
     private static final String KEY_SERVER_IP = "server_ip_v3";
     private static final String KEY_SERVER_PORT = "server_port_v3";
-    private static final String DEFAULT_IP = "shinkansen.proxy.rlwy.net";
-    private static final int DEFAULT_PORT = 53312;
+    private static final String DEFAULT_IP = "149.248.194.8";
+    private static final int DEFAULT_PORT = 12345;
 
     /**
      * Obtiene la IP del servidor guardada o retorna localhost
